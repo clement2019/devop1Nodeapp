ac7d2bbe5aa1824722c9c747045e8bcf3c3074e8
Ayeni Clement <ayeniayokunle@gmail.com>
2022-04-24 22:38:13 +0100

    just added all the files

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const app=express();
+const PORT = 3000
 app.get('/',(req,res) => {
-res.send('Application is running');
+res.send('Application is running on adeyemi porter');
 
 });
 
-app.listen(PORT);
+app.listen(3000);
 console.log('server running on port: 3000');
